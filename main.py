a54a732f50d7965d7ee8efccc95a2d4743a9674a
glowystars <98676785+astro-chloe@users.noreply.github.com>
2022-01-30 03:10:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,137 @@
+from flask import Flask, render_template, request
+import requests, json, xmltodict
+
+app = Flask(
+	__name__,
+	template_folder = 'templates',
+	static_folder = 'static'
+)
+
+@app.route("/")
+def index():
+  res = requests.get("https://inciweb.nwcg.gov/feeds/rss/incidents/")
+  data = xmltodict.parse(res.content)
+  data = data["rss"]["channel"]["item"]
+
+  activeWildfires = []
+
+  for item in data:
+    if not "Prescribed Fire" in item["title"] and not "Burned Area Emergency Response" in item["title"]:
+      trimmedTitle = item["title"].rpartition(' (')[0]
+
+      lat = item["geo:lat"]
+      long = item["geo:long"]
+      res = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + ", " + long + "&key=AIzaSyD2ZWo9VogormcvsIxF3U8e0yCB84O0p8c")
+      data = res.json()
+      formattedAddress = data["results"][0]["formatted_address"]
+      trimmedFormattedAddress = formattedAddress[:len(formattedAddress) - 5]
+      if (trimmedFormattedAddress[len(trimmedFormattedAddress)-1].isdigit()):
+        trimmedFormattedAddress = trimmedFormattedAddress[:len(trimmedFormattedAddress) - 6]
+      
+      strippedFormattedAddress = ""
+      numSpaces = 0
+      for c in trimmedFormattedAddress[::-1]:
+        if c == " ":
+          numSpaces += 1
+        if numSpaces < 2:
+          strippedFormattedAddress += c
+
+      strippedFormattedAddress = strippedFormattedAddress[::-1]
+      
+      if len(strippedFormattedAddress) > 3:
+        wildfireInfo = trimmedTitle + " — " + strippedFormattedAddress
+        activeWildfires.append(wildfireInfo)
+        
+  activeWildfires = sorted(activeWildfires, key = lambda x:x[-2])
+
+  return render_template("index.html", wildfires=activeWildfires)
+
+@app.route("/location", methods=["POST"])
+def location():
+  res = requests.get("https://maps.googleapis.com/maps/api/geocode/json?components=country:US%7Cpostal_code:" + request.form.get("zip-code", "94010") + "&key=AIzaSyD2ZWo9VogormcvsIxF3U8e0yCB84O0p8c")
+  data = res.json()
+  formattedAddress = data["results"][0]["formatted_address"]
+
+  trimmedFormattedAddress = ""
+  for c in formattedAddress:
+    if c.isdigit():
+      trimmedFormattedAddress = trimmedFormattedAddress[:-1]
+      break  
+    trimmedFormattedAddress += c
+  
+  state = trimmedFormattedAddress[-2:]
+
+  res = requests.get("https://inciweb.nwcg.gov/feeds/rss/incidents/")
+  data = xmltodict.parse(res.content)
+  data = data["rss"]["channel"]["item"]
+
+  nearbyWildfires = []
+
+  for item in data:
+    if not "Prescribed Fire" in item["title"] and not "Burned Area Emergency Response" in item["title"]:
+      trimmedTitle = item["title"].rpartition(' (')[0]
+
+      lat = item["geo:lat"]
+      long = item["geo:long"]
+      res2 = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + ", " + long + "&key=AIzaSyD2ZWo9VogormcvsIxF3U8e0yCB84O0p8c")
+      data2 = res2.json()
+      formattedAddress2 = data2["results"][0]["formatted_address"]
+      trimmedFormattedAddress2 = formattedAddress2[:len(formattedAddress2) - 5]
+      if (trimmedFormattedAddress2[len(trimmedFormattedAddress2)-1].isdigit()):
+        trimmedFormattedAddress2 = trimmedFormattedAddress2[:len(trimmedFormattedAddress2) - 6]
+      
+      strippedFormattedAddress = ""
+      numSpaces = 0
+      for c in trimmedFormattedAddress2[::-1]:
+        if c == " ":
+          numSpaces += 1
+        if numSpaces < 2:
+          strippedFormattedAddress += c
+
+      strippedFormattedAddress = strippedFormattedAddress[::-1]
+      
+      if len(strippedFormattedAddress) > 3 and state in strippedFormattedAddress:
+        wildfireInfo = trimmedTitle + " — " + strippedFormattedAddress
+        nearbyWildfires.append(wildfireInfo)
+        
+  nearbyWildfires = sorted(nearbyWildfires, key = lambda x:x[-2])
+
+  return render_template("location.html", address=trimmedFormattedAddress, wildfires=nearbyWildfires)
+
+@app.route("/emergency-kit")
+def emergencyKit():
+  return render_template("emergency-kit.html")
+
+@app.route("/emergency-plan")
+def emergencyPlan():
+  return render_template("emergency-plan.html")
+
+@app.route("/agriculture")
+def agriculture():
+  return render_template("agriculture.html")
+
+@app.route("/child-safety")
+def childSafety():
+  return render_template("child-safety.html")
+
+@app.route("/pet-safety")
+def petSafety():
+  return render_template("pet-safety.html")
+
+@app.route("/prepare-for-evacuation")
+def prepareForEvacuation():
+  return render_template("prepare-for-evacuation.html")
+
+@app.route("/evacuation")
+def evacuation():
+  return render_template("evacuation.html")
+
+@app.route("/returning-home")
+def returningHome():
+  return render_template("returning-home.html")
+
+if __name__ == "__main__":
+	app.run(
+		host='0.0.0.0',
+		port=1000
+	)
